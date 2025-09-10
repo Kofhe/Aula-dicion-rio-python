607d63999d4d4e458bcce6adf9c5335c95d2f59a
Andreia Dias <andreiadias2311@gmail.com>
2025-09-10 18:28:15 -0300

    set.py

---
+++ set.py
@@ -0,0 +1,13 @@
+a = {1,2,3,4,5,6,7,}
+b = {4,5,6}
+
+clientes = ["Gabriel", "João", "Gabriel"]
+clientes = set(clientes)transformando a lista em um set
+
+# Para adicionar, limpar e remover respectivamente
+# a.add()
+# a.clear()
+# a.remove()
+# mostrar a interseção de a e b
+# mostrar a união de a e b
+Print(clientes)
