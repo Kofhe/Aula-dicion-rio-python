ae182208ecaf956e869477752e781a7ae0ef00e5
Andreia Dias <andreiadias2311@gmail.com>
2025-09-10 18:15:16 -0300

    EXERCÍCIOS_DICIONÁRIOS.py

---
+++ EXERCÍCIOS_DICIONÁRIOS.py
@@ -0,0 +1,21 @@
+# EXERCÍCIOS DICIONÁRIOS
+
+# 1 - definam um dicionário vazio
+# - adicione um novo par de chave-valor com a chave nome e seu nome completo
+# - adicione um novo par de chave-valor com a chave email
+# - adicione um novo par de chave-valor com a chave telefone 
+# - adicione um novo par de chave-valor com a chave profissao 
+
+# - Recebam do usuário através do "input()" esse nome, email, telefone e profissão.
+
+# 2 - Atualize o valor de email para "teste@teste.com"
+
+# Projeto Guiado 
+
+# - faça lista vazia chamada "clientes" ---> no plural
+# - Faça um dicionário vazio chamado "cliente" --> no singular
+# - em seguida, peça nome para o usuário (utilize o input( ) ) e armazene no dicionário "cliente"
+# - em seguida, peça telefone para o usuário (utilize o input( ) ) e armazene no dicionário "cliente"
+# - em seguida, peça email para o usuário (utilize o input( ) ) e armazene no dicionário "cliente"
+# - em seguida, peça profissao para o usuário (utilize o input( ) ) e armazene no dicionário "cliente"
+# - Por fim, adicione esse dicionário na lista "clientes" com o método append()
