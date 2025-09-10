edcb795931db13c6d89d9de482156c311b779f44
Andreia Dias <andreiadias2311@gmail.com>
2025-09-10 17:56:42 -0300

    dicionario.py

---
+++ dicionario.py
@@ -0,0 +1,13 @@
+cliente_dicionario = {
+    "nome":"Gabriel",
+    "nacionalidade":"Brasileiro",
+    "idade":21,
+    "imc":34,
+    "saldo_bancario":50.5
+    }
+print(cliente_dicionario["nome"])
+print(cliente_dicionario["nacionalidade"])
+print(cliente_dicionario["idade"])
+print(cliente_dicionario["imc"])
+print(cliente_dicionario["saldo_bancario"])
+print(cliente_dicionario.get("nome"))
